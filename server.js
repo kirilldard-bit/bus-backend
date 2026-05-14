0078da74422d1d5c18ec53b8aed560b0640d158a
kirilldard-bit <kirilldard@gmail.com>
2026-05-14 13:49:47 +0300

    working backend

--- server.js
+++ server.js
@@ -6,20 +6,9 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
+
   res.json({
     status: 'BUSTER backend running'
-  });
-});
-
-app.get('/check-access', (req, res) => {
-
-  console.log(
-    'Checking access for:',
-    req.query.user
-  );
-
-  res.json({
-    access: false
   });
 
 });
