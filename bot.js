a27e826ffd209233d149294d2aded31b10ff2edd
kirilldard-bit <kirilldard@gmail.com>
2026-05-18 20:15:34 +0300

    add telegram bot

---
+++ bot.js
@@ -0,0 +1,54 @@
+const { Telegraf, Markup } =
+  require('telegraf');
+
+const bot =
+  new Telegraf(
+    process.env.BOT_TOKEN
+  );
+
+bot.start(async (ctx) => {
+
+  await ctx.replyWithPhoto(
+
+    {
+      url:
+        'https://buster-app-three.vercel.app/bg2.png'
+    },
+
+    {
+
+      caption:
+`🚀 Добро пожаловать в BUSTER
+
+Инструмент для водителей нового поколения.
+
+• аналитика
+• маршруты
+• статистика
+• настройки профиля
+
+Нажми кнопку ниже чтобы открыть приложение.`,
+
+      ...Markup.inlineKeyboard([
+
+        Markup.button.webApp(
+
+          '🚀 ОТКРЫТЬ BUSTER',
+
+          'https://buster-app-three.vercel.app'
+
+        )
+
+      ])
+
+    }
+
+  );
+
+});
+
+bot.launch();
+
+console.log(
+  'TELEGRAM BOT STARTED'
+);
