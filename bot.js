f626f52b053a06c335c1d12ed766e67c98cdddef
kirilldard-bit <kirilldard@gmail.com>
2026-05-18 21:12:06 +0300

    add telegram welcome image

--- bot.js
+++ bot.js
@@ -1,30 +1,30 @@
-const { Telegraf, Markup } = require('telegraf');
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
 bot.start(async (ctx) => {
 
-  await ctx.reply(
+  await ctx.replyWithPhoto(
 
+    {
+      url: 'https://buster-app-three.vercel.app/assets/welcome.png'
+    },
+
+    {
+      caption:
 `🚀 Добро пожаловать в BUSTER
 
 Помощник для водителей нового поколения.
 
 Нажми кнопку ниже чтобы открыть приложение.`,
 
-    Markup.inlineKeyboard([
-      [
-        Markup.button.webApp(
-          '🚀 ОТКРЫТЬ BUSTER',
-          'https://buster-app-three.vercel.app'
-        )
-      ]
-    ])
+      ...Markup.inlineKeyboard([
+        [
+          Markup.button.webApp(
+            '🚀 ОТКРЫТЬ BUSTER',
+            'https://buster-app-three.vercel.app'
+          )
+        ]
+      ])
+
+    }
 
   );
 
 });
-
-bot.launch();
-
-console.log('TELEGRAM BOT STARTED');
